709bee652afa0f6d381bfc7298bcd3d6c3481e15
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-03-31 08:49:24 +0000

    create async await func that takes data from api fetch, create global questions variable

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
 //assign the api URL to the url variable. provides 10 multiple choice, multiple difficulty questions per call.
 const url = "https://opentdb.com/api.php?amount=10&type=multiple";
 
+//globally declare the shuffled questions array so they can be accessed and used in the main.js file. 
+let questions = []
+
 //Fetch API allows web browser to make HTTP requests
 //returns a response result for date of an array of questions.
 fetch(`${url}`)
@@ -8,3 +11,16 @@ fetch(`${url}`)
   .then((data) => {
     quizAPI(data);//pass retreived data to the quizAPi function
 })
+
+async function quizAPI(data) {
+    tempArray = [];//create a temporary array
+    await data.results.map((item) => { //await makes the function wait for a promise.
+    const { correct_answer, incorrect_answers } = item;
+    tempArray = [correct_answer, ...incorrect_answers];//pass correct ans and copy of incorrect ans to the temp array
+    item.possible_answers = [...tempArray];//copy the temp array to the possible answers of item 
+    questions.push(item); //push item to the global questions variable    
+  });
+//pass the questions to the shuffle function
+  shuffleQuestions(questions); 
+   
+}
