ac41c5f624857479e019cbce49d69999cbbf6488
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-03-31 09:06:38 +0000

    selecting the required DOM elements

---
+++ main.js
@@ -0,0 +1,12 @@
+//selecting required DOM elements
+const welcome = document.querySelector(".welcome");
+const startBtn = document.querySelector(".startBtn button");
+const infoBox = document.querySelector(".infoBox");
+const exitBtn = infoBox.querySelector(".buttons .quit");
+const continueBtn = infoBox.querySelector(".buttons .restart");
+const quizBox = document.querySelector(".quizBox");
+const resultBox = document.querySelector(".resultBox");
+const optionList = document.querySelector(".optionList");
+const timeLine = document.querySelector("header .timeLine");
+const timeText = document.querySelector(".timer .timeLeftText");
+const timeCount = document.querySelector(".timer .timerSeconds");
