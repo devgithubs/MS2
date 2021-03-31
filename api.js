dd708474f5df00891b0e990dd1870746e83d81a5
devgithubs <arthur.oleary24@mail.dcu.ie>
2021-03-31 08:43:59 +0000

    create fetch method to make http request to opendb api

--- api.js
+++ api.js
@@ -1,2 +1,10 @@
 //assign the api URL to the url variable. provides 10 multiple choice, multiple difficulty questions per call.
 const url = "https://opentdb.com/api.php?amount=10&type=multiple";
+
+//Fetch API allows web browser to make HTTP requests
+//returns a response result for date of an array of questions.
+fetch(`${url}`)
+  .then((response) => response.json())
+  .then((data) => {
+    quizAPI(data);//pass retreived data to the quizAPi function
+})
